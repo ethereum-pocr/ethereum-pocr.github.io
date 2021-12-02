6fbd2e1a10941eb7bdcccbe37455e7b8285d2863
Guenole de Cadoudal <guenoledc@yahoo.fr>
2021-12-02 18:48:06 +0100

    add all nodes in bootnodes

--- enode.sh
+++ enode.sh
@@ -1,5 +1,5 @@
 BOOTNODEKEY=$(bootnode -writeaddress -nodekey /node/data/geth/nodekey)
-PUBLIC_IP=$(curl ifconfig.me/ip)
+PUBLIC_IP=$(curl -s ifconfig.me/ip)
 PORT=30303
 
 echo "enode://$BOOTNODEKEY@$PUBLIC_IP:$PORT"
--- start-other-node.sh
+++ start-other-node.sh
@@ -1,16 +1,26 @@
-FIRST_NODE_ENODE_URL=https://saturn-pocr-1.uksouth.cloudapp.azure.com/enode
-BOOTNODE=$(curl -s $FIRST_NODE_ENODE_URL)
-# by default will be the first created account - ensure you have only one
+ENODE_URLS="https://saturn-pocr-1.uksouth.cloudapp.azure.com/enode https://saturn-pocr-2.swedencentral.cloudapp.azure.com/enode https://saturn-pocr-3.francecentral.cloudapp.azure.com/enode"
+
+BOOTNODES=""
+for url in $ENODE_URLS
+do
+echo "look for $url"
+  BOOTNODES="$(curl -s $url),$BOOTNODES"
+done
+
+# by default will be the first created account 
+# - ensure you have only one or change the #0 by the appropriate index
+# - or replace the value by the account you want to use
 COINBASE=$(geth account list --keystore ./keystore/ | grep "#0" | sed -nr 's/^.+\{(.+)\}.+$/\1/p')
 
 DATADIR=/node/data
 KEYSTORE=/node/keystore
 NET_ID=1974
 GETH=geth-pocr
-EXT_API='eth,net,web3,clique'
-INT_API=$EXT_API,admin,personal,txpool
+EXT_API='eth,net,web3'
+# ipc is enabling all the below by default, no need
+INT_API=$EXT_API,admin,personal,txpool,clique
 
 
-nohup $GETH --datadir $DATADIR --keystore $KEYSTORE --syncmode 'full' --port 30303 --http --http.addr '0.0.0.0' --http.corsdomain "*" --http.port 8545  --http.vhosts=* --http.api $EXT_API --ws --ws.port 8546 --networkid $NET_ID --miner.gasprice '1000000000' --bootnodes $BOOTNODE  --allow-insecure-unlock --password $KEYSTORE/password.txt --unlock $COINBASE --mine 2>&1 >> /node/geth.log &
+nohup $GETH --datadir $DATADIR --keystore $KEYSTORE --syncmode 'full' --port 30303 --http --http.addr '0.0.0.0' --http.corsdomain "*" --http.port 8545  --http.vhosts=* --http.api $EXT_API --ws --ws.port 8546 --networkid $NET_ID --miner.gasprice '1000000000' --bootnodes $BOOTNODES  --allow-insecure-unlock --password $KEYSTORE/password.txt --unlock $COINBASE --mine 2>&1 >> /node/geth.log &
 
 
