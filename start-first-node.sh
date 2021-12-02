06fc5b7ae89533cd0f094112a32e188271946e3b
Guenole de Cadoudal <guenoledc@yahoo.fr>
2021-12-02 17:24:47 +0100

    add the nohup

--- start-first-node.sh
+++ start-first-node.sh
@@ -10,5 +10,5 @@ EXT_API='eth,net,web3,clique'
 INT_API=$EXT_API,admin,personal,txpool
 
 
-$GETH --datadir $DATADIR --keystore $KEYSTORE --syncmode 'full' --port 30303 --http --http.addr '0.0.0.0' --http.corsdomain "*" --http.port 8545  --http.vhosts=* --http.api $EXT_API --ws --ws.port 8546 --networkid $NET_ID --miner.gasprice '1000000000' --allow-insecure-unlock -unlock $COINBASE --password $KEYSTORE/password.txt --mine
+nohup $GETH --datadir $DATADIR --keystore $KEYSTORE --syncmode 'full' --port 30303 --http --http.addr '0.0.0.0' --http.corsdomain "*" --http.port 8545  --http.vhosts=* --http.api $EXT_API --ws --ws.port 8546 --networkid $NET_ID --miner.gasprice '1000000000' --allow-insecure-unlock -unlock $COINBASE --password $KEYSTORE/password.txt --mine 2>&1 >> /node/geth.log &
 
--- start-other-node.sh
+++ start-other-node.sh
@@ -10,5 +10,7 @@ EXT_API='eth,net,web3,clique'
 INT_API=$EXT_API,admin,personal,txpool
 
 
-$GETH --datadir $DATADIR --keystore $KEYSTORE --syncmode 'full' --port 30303 --http --http.addr '0.0.0.0' --http.corsdomain "*" --http.port 8545  --http.vhosts=* --http.api $EXT_API --ws --ws.port 8546 --networkid $NET_ID --miner.gasprice '1000000000' --bootnodes $BOOTNODE  --allow-insecure-unlock --password $KEYSTORE/password.txt --mine # -unlock $COINBASE 
+nohup $GETH --datadir $DATADIR --keystore $KEYSTORE --syncmode 'full' --port 30303 --http --http.addr '0.0.0.0' --http.corsdomain "*" --http.port 8545  --http.vhosts=* --http.api $EXT_API --ws --ws.port 8546 --networkid $NET_ID --miner.gasprice '1000000000' --bootnodes $BOOTNODE  --allow-insecure-unlock --password $KEYSTORE/password.txt --mine 2>&1 >> /node/geth.log &
+
+ # -unlock $COINBASE 
 
