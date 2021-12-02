2702d3daafd353008e9cdeab969f9dc6616b4a29
Guenole de Cadoudal <guenoledc@yahoo.fr>
2021-12-02 17:41:19 +0100

    force unlock first account

--- start-other-node.sh
+++ start-other-node.sh
@@ -1,6 +1,7 @@
 FIRST_NODE_ENODE_URL=https://saturn-pocr-1.uksouth.cloudapp.azure.com/enode
 BOOTNODE=$(curl -s $FIRST_NODE_ENODE_URL)
-# COINBASE=by default will be the first created account - ensure you have only one
+# by default will be the first created account - ensure you have only one
+COINBASE=$(geth account list --keystore ./keystore/ | grep "#0" | sed -nr 's/^.+\{(.+)\}.+$/\1/p')
 
 DATADIR=/node/data
 KEYSTORE=/node/keystore
@@ -10,7 +11,6 @@ EXT_API='eth,net,web3,clique'
 INT_API=$EXT_API,admin,personal,txpool
 
 
-nohup $GETH --datadir $DATADIR --keystore $KEYSTORE --syncmode 'full' --port 30303 --http --http.addr '0.0.0.0' --http.corsdomain "*" --http.port 8545  --http.vhosts=* --http.api $EXT_API --ws --ws.port 8546 --networkid $NET_ID --miner.gasprice '1000000000' --bootnodes $BOOTNODE  --allow-insecure-unlock --password $KEYSTORE/password.txt --mine 2>&1 >> /node/geth.log &
+nohup $GETH --datadir $DATADIR --keystore $KEYSTORE --syncmode 'full' --port 30303 --http --http.addr '0.0.0.0' --http.corsdomain "*" --http.port 8545  --http.vhosts=* --http.api $EXT_API --ws --ws.port 8546 --networkid $NET_ID --miner.gasprice '1000000000' --bootnodes $BOOTNODE  --allow-insecure-unlock --password $KEYSTORE/password.txt --unlock $COINBASE --mine 2>&1 >> /node/geth.log &
 
- # -unlock $COINBASE 
 
