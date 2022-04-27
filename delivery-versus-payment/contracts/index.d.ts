ce91087c51758e7652c2f56c1deb1970a11ea7eb
mdeko <mdekoninck@finaxys.com>
2022-04-27 14:36:19 +0200

    chore(cleanup): remove generated folder after smart contract compilation

--- delivery-versus-payment/contracts/index.d.ts
+++
@@ -1,3 +0,0 @@
-import { SmartContracts } from "@saturn-chain/smart-contract"
-declare const _default: SmartContracts;
-export default _default;
--- delivery-versus-payment/contracts/index.js
+++
@@ -1,3 +0,0 @@
-const {SmartContracts} = require("@saturn-chain/smart-contract");
-const combined = require("./combined.json");
-module.exports = SmartContracts.load(combined);
