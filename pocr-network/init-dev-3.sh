a2802838851bcb0a1237f4ce94c102f799063a38
Guenole de Cadoudal <guenoledc@yahoo.fr>
2022-12-03 08:11:09 +0100

    Doc and fix dev scripts

--- pocr-network/init-dev-3.sh
+++ pocr-network/init-dev-3.sh
@@ -1,7 +1,7 @@
 
 NODES="node1 node2 node3"
 CURDIR=`pwd`
-# GENESIS=$CURDIR/genesis/saturndev-sealer1-authorized.json
+GENESIS_CONF=$CURDIR/genesis/local-conf.json
 GENESIS=$CURDIR/genesis/saturndev-all-sealers-authorized.json
 
 # force recompile the smart contracts and inject into the genesis
@@ -9,7 +9,7 @@ cd ../sc-carbon-footprint
 source ./build.sh
 
 export SEALERS="0x6e45c195e12d7fe5e02059f15d59c2c976a9b730 0x926ed993bf6a57306a7dc5ef2f6c2053da42f85c 0xcda0bd40e7325f519f31bb3f31f68bc7d4c78903"
-node prepare-genesis.js $GENESIS
+node prepare-genesis.js $GENESIS_CONF > $GENESIS
 
 GETH=/Users/guenole/VSCode/saturn/github/go-ethereum/build/bin/geth 
 $GETH version
