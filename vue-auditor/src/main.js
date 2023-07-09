797ebc8442f6832ae113c962e2599df05036901c
Guenole de Cadoudal <guenoledc@yahoo.fr>
2023-07-09 12:20:05 +0200

    Revert to previous buildable code

--- vue-auditor/src/main.js
+++ vue-auditor/src/main.js
@@ -8,10 +8,12 @@ import '@mdi/js'
 import store from "./store/index"
 import router from './router'
 import { setupAuthNavigationGuard } from '@/lib/initialization'
+import { trackPageView } from '@/lib/track';
 
 Vue.config.productionTip = false
 
 setupAuthNavigationGuard(router, store);
+trackPageView();
 
 new Vue({
   vuetify,
